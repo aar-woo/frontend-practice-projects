9faf76e24370088467f5fc32a0426f23514dfba5
Aaron Woo <awoo@chapman.edu>
2022-05-20 15:22:03 -0700

    Completed creation of static profCard componenent in mobile view.

--- time-tracking-dashboard/time-tracking-dashboard-main/react-time-tracking-dashboard/src/Components/ProfileCard/profileCard.js
+++ time-tracking-dashboard/time-tracking-dashboard-main/react-time-tracking-dashboard/src/Components/ProfileCard/profileCard.js
@@ -2,18 +2,18 @@ import JeremyProf from "../../images/image-jeremy.png";
 import styles from './profileCard.module.css';
 export default function ProfileCard() {
     return (
-        <div className="prof-card">
-            <div className={`${styles.profBg} d-flex`}>
+        <div className={styles.profCard}>
+            <div className={`${styles.profBg} row w-100 m-0`}>
                 <div className="col-4 p-3" >
-                    <img src={JeremyProf} className="w-100"/>  
+                    <img src={JeremyProf} className="w-100 border rounded-circle "/>  
                 </div>
                 <div className="col-8 d-flex flex-column align-items-start justify-content-center" >
                     <p className="m-0">Report for</p>
-                    <h2>Jeremy Robson</h2>
+                    <h2 className='fw-normal text-white'>Jeremy Robson</h2>
                 </div>
             </div>
-            <div className="row">
-                <ul className="list-unstyled d-flex justify-content-around">
+            <div className={`${styles.intervals} row align-items-center`}>
+                <ul className="list-unstyled m-0 d-flex justify-content-around align-items-center">
                     <li>Daily</li>
                     <li>Weekly</li>
                     <li>Montly</li>
