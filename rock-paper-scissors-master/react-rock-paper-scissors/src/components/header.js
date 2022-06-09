5841acbb26c19a9ceb65b536ed1b9e469865c85f
Aaron Woo <awoo@chapman.edu>
2022-06-09 15:39:48 -0700

    Addes sass and reassigned bootstrap default border color.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/header.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/header.js
@@ -1,13 +1,13 @@
 import styles from './header.module.css';
 export default function Header(props) {
     return (
-        <div className="row m-4 p-2 d-flex justify-content-between border border-3 rounded">
+        <div className={`${styles.headerOutline} row m-4 p-2 d-flex justify-content-between border border-3 rounded`}>
             <div className="col-4 text-start d-flex align-items-center"> 
-                <h5 className="mb-0 lh-1">ROCK PAPER SCISSORS</h5>
+                <h4 className="mb-0 lh-1 text-white fw-bold">ROCK PAPER SCISSORS</h4>
             </div>
-            <div className={`${styles.score} col-4 border bg-light rounded d-flex flex-wrap justify-content-center align-content-center`}>
+            <div className={`${styles.score} py-4 col-4 border bg-light rounded d-flex flex-wrap justify-content-center align-content-center`}>
                 <p className="m-0">SCORE</p>
-                <p className="fs-1 fw-bold m-0">12</p>
+                <h1 className={`${styles.scoreNum} fw-bold m-0`}>12</h1>
             </div>
         </div>
     )
