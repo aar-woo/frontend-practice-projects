bc583624963f1f429dca210f4015fa4d4ef170db
Aaron Woo <awoo@chapman.edu>
2022-06-09 14:57:51 -0700

    Added background color radial gradient to body.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/App.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-primary.bg-gradient">
       <Header />
     </div>
   );
