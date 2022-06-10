e59d25a3f7f03632553c450aabd0fbf41277c6d1
Aaron Woo <awoo@chapman.edu>
2022-06-10 15:32:55 -0700

    Moved images to src folder in react project and created files for GameOption component.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/App.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/header';
+import Header from './components/Header/header';
+import GameOption from './components/GameOption/gameOption';
 
 function App() {
   return (
     <div className="App bg-primary.bg-gradient">
       <Header />
+      <GameOption />
     </div>
   );
 }
---
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
@@ -0,0 +1,7 @@
+import rockIcon from '../../../public/images/icon-rock'
+
+export default function GameOption(props) {
+    return (
+        <img src={rockIcon} />
+    )
+}
