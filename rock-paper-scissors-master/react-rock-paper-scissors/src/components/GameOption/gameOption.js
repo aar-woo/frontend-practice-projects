b5dfd12938337ea5208d9165951a42cb647160f2
Aaron Woo <awoo@chapman.edu>
2022-06-10 15:47:02 -0700

    Added blue outer circle to around icon and resized option icon.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
@@ -2,8 +2,11 @@ import rockIcon from '../../images/icon-rock.svg';
 import styles from './gameOption.module.css';
 export default function GameOption(props) {
     return (
-        <div className={`${styles.optionInnerCircle} bg-white border rounded-circle d-flex justify-content-center align-items-center`}>
-            <img src={rockIcon} className="w-50 h-50"/>
+        <div className={`${styles.optionOuterCircle} bg-primary border rounded-circle d-flex justify-content-center align-items-center`}>
+            <div className={`${styles.optionInnerCircle} bg-white border rounded-circle d-flex justify-content-center align-items-center`}>
+                <img src={rockIcon} className="w-40 h-40"/>
+            </div>
         </div>
+       
     )
 }
