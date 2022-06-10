fd35de7ae48a785fcdedd3e762a6d013157e392e
Aaron Woo <awoo@chapman.edu>
2022-06-10 15:25:14 -0700

    Moved image folder into react public folder,changed bootstrap line-height-base class to adjust game title css, adjusted score and num css.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/header.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/header.js
@@ -3,11 +3,11 @@ export default function Header(props) {
     return (
         <div className={`${styles.headerOutline} row m-4 p-2 d-flex justify-content-between border border-3 rounded`}>
             <div className="col-4 text-start d-flex align-items-center"> 
-                <h4 className="mb-0 lh-1 text-white fw-bold">ROCK PAPER SCISSORS</h4>
+                <h3 className="mb-0 lh-base text-white fw-bold">ROCK PAPER SCISSORS</h3>
             </div>
-            <div className={`${styles.score} py-4 col-4 border bg-light rounded d-flex flex-wrap justify-content-center align-content-center`}>
-                <p className="m-0">SCORE</p>
-                <h1 className={`${styles.scoreNum} fw-bold m-0`}>12</h1>
+            <div className={`${styles.score} py-5 col-4 border bg-light rounded d-flex flex-wrap justify-content-center align-content-center`}>
+                <p className="m-0 lh-1">SCORE</p>
+                <h1 className={`${styles.scoreNum} m-0 lh-1 d-flex align-self-start`}>12</h1>
             </div>
         </div>
     )
