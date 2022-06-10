617741127c52ed30d96d9380ac7ac1be6f019a15
Aaron Woo <awoo@chapman.edu>
2022-06-10 15:43:03 -0700

    Added white inner circle around option icon and resized both.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
@@ -1,7 +1,9 @@
 import rockIcon from '../../images/icon-rock.svg';
-
+import styles from './gameOption.module.css';
 export default function GameOption(props) {
     return (
-        <img src={rockIcon} />
+        <div className={`${styles.optionInnerCircle} bg-white border rounded-circle d-flex justify-content-center align-items-center`}>
+            <img src={rockIcon} className="w-50 h-50"/>
+        </div>
     )
 }
