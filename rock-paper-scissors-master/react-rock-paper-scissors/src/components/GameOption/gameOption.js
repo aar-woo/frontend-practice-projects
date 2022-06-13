43e15c0a2b8190efbee668875a5a792f8d97df1b
Aaron Woo <awoo@chapman.edu>
2022-06-13 15:27:26 -0700

    Changed rock icon background to correct color and added radial-gradient to it.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
@@ -2,7 +2,7 @@ import rockIcon from '../../images/icon-rock.svg';
 import styles from './gameOption.module.css';
 export default function GameOption(props) {
     return (
-        <div className={`${styles.optionOuterCircle} bg-primary border rounded-circle d-flex justify-content-center align-items-center`}>
+        <div className={`${styles.optionOuterCircle} border rounded-circle d-flex justify-content-center align-items-center`}>
             <div className={`${styles.optionInnerCircle} bg-white border rounded-circle d-flex justify-content-center align-items-center`}>
                 <img src={rockIcon} className="w-40 h-40"/>
             </div>
