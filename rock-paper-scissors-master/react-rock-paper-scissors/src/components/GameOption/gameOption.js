bac31ac8b5c6d69ca4d178128f1539efef98d9c3
Aaron Woo <awoo@chapman.edu>
2022-06-13 15:45:51 -0700

    Updated gameOption component to be dynamic and take in a hand props to determine its icon and backgroud styling.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
@@ -1,10 +1,22 @@
 import rockIcon from '../../images/icon-rock.svg';
+import paperIcon from '../../images/icon-paper.svg';
+import scissorsIcon from '../../images/icon-scissors.svg';
+
 import styles from './gameOption.module.css';
 export default function GameOption(props) {
+    const {hand} = props;
+    const handToIconObj = {
+        rock: rockIcon,
+        paper: paperIcon,
+        scissors: scissorsIcon
+    }
+    const icon = handToIconObj[hand]
+
+    const optionClass = `${hand}Outer`
     return (
-        <div className={`${styles.optionOuterCircle} border rounded-circle d-flex justify-content-center align-items-center`}>
-            <div className={`${styles.optionInnerCircle} bg-white border rounded-circle d-flex justify-content-center align-items-center`}>
-                <img src={rockIcon} className="w-40 h-40"/>
+        <div className={`${styles[`${hand}OuterCircle`]} border-none rounded-circle d-flex justify-content-center align-items-center`}>
+            <div className={`${styles[`${hand}InnerCircle`]} bg-white border-none rounded-circle d-flex justify-content-center align-items-center`}>
+                <img src={icon} className="w-40 h-40"/>
             </div>
         </div>
        
