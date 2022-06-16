79257da9382ee275899e085f1f77a0f9a8cd9000
Aaron Woo <awoo@chapman.edu>
2022-06-16 15:49:30 -0700

    Added state to handle modal state.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/App.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 import Header from './components/Header/header';
 import Options from './components/OptionsComponent/options';
@@ -6,8 +6,9 @@ import Button from './components/Button/button';
 import Modal from './components/Modal/modal';
 
 function App() {
+  const [modalIsOpen, toggleModal] = useState(false);
+  
   return (
-    
     <div className="App bg-primary.bg-gradient p-5 py-4 d-flex flex-column">
         <Header />
         <Options />
--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/Modal/modal.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/Modal/modal.js
@@ -4,11 +4,11 @@ import rulesImg from '../../images/image-rules.svg'
 import closeIcon from '../../images/icon-close.svg';
 export default function Modal(props) {
     return (
-        <div className={`${styles.modal} container-fluid h-100 bg-white d-flex flex-column align-items-center
+        <div className={`${styles.modal} d-none container-fluid h-100 bg-white d-flex flex-column align-items-center
         justify-content-around py-3`}>
             <h1 className={`${styles.modalTitle} fw-bold`}>RULES</h1>
             <img src={rulesImg} className=''/>
-            <img src={closeIcon} className={styles.closeIcon}/>
+            <img src={closeIcon} className={styles.closeIcon} />
         </div>
     )
 }
