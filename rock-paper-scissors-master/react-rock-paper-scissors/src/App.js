bf84c3e6e9835c00288242533da8cfe05d03e781
Aaron Woo <awoo@chapman.edu>
2022-06-16 15:58:59 -0700

    Create toggle function for modal and passed as props to button and modal component, added onClick event listener to buttosn to change modal state allowing it to open and close.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/App.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/App.js
@@ -6,14 +6,17 @@ import Button from './components/Button/button';
 import Modal from './components/Modal/modal';
 
 function App() {
-  const [modalIsOpen, toggleModal] = useState(false);
-  
+  const [modalIsOpen, setModalState] = useState(false);
+  const toggleModal = function() {
+    setModalState(!modalIsOpen);
+  }
+
   return (
     <div className="App bg-primary.bg-gradient p-5 py-4 d-flex flex-column">
         <Header />
         <Options />
-        <Button type='rules' />
-        <Modal />
+        <Button type='rules' toggleModal={toggleModal} />
+        <Modal modalIsOpen={modalIsOpen} toggleModal={toggleModal} />
     </div>
   );
 }
--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/Button/button.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/Button/button.js
@@ -1,9 +1,9 @@
 import styles from './button.module.css';
 
 export default function Button(props) {
-    const { type } = props;
+    const { type, toggleModal } = props;
 
     return (
-        <button type="button" className={`${styles[type]} btn btn-outline-light btn-lg fw-lighter `}>{type.toUpperCase()}</button>
+        <button type="button" className={`${styles[type]} btn btn-outline-light btn-lg fw-lighter`} onClick={toggleModal}>{type.toUpperCase()}</button>
     )
 }
