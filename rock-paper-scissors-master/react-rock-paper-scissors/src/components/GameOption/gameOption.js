16326f348baa357acaf22cb0a0fa336c2a4ef364
Aaron Woo <awoo@chapman.edu>
2022-06-16 15:37:01 -0700

    Removed changes to gameComponent made for creating rules icons after realizing the modal uses an image.

--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/GameOption/gameOption.js
@@ -2,29 +2,22 @@ import rockIcon from '../../images/icon-rock.svg';
 import paperIcon from '../../images/icon-paper.svg';
 import scissorsIcon from '../../images/icon-scissors.svg';
 
+
 import styles from './gameOption.module.css';
 export default function GameOption(props) {
-    const {hand, mode} = props;
+    const {hand} = props;
     const handToIconObj = {
         rock: rockIcon,
         paper: paperIcon,
         scissors: scissorsIcon
     }
     
-    let outerCircleClass = `${hand}OuterCircle`;
-    let innerCircleClass = 'innerCircle';
-
-    if (mode === 'rules') {
-        outerCircleClass = 'rulesOuterCircle';
-        innerCircleClass = 'rulesInnerCircle';
-    }
-    
     const icon = handToIconObj[hand]
 
     return (
-        <div className={`${styles[outerCircleClass]} border-none rounded-circle d-flex justify-content-center align-items-center`}>
-            <div className={`${styles[innerCircleClass]} border-none rounded-circle d-flex justify-content-center align-items-center`}>
-                <img src={icon} className="w-50"/>
+        <div className={`${styles[`${hand}OuterCircle`]} border-none rounded-circle d-flex justify-content-center align-items-center`}>
+            <div className={`${styles.innerCircle} border-none rounded-circle d-flex justify-content-center align-items-center`}>
+                <img src={icon} className='w-50'/>
             </div>
         </div>
        
--- rock-paper-scissors-master/react-rock-paper-scissors/src/components/Modal/modal.js
+++ rock-paper-scissors-master/react-rock-paper-scissors/src/components/Modal/modal.js
@@ -1,10 +1,11 @@
 import styles from './modal.module.css';
 import GameOption from '../GameOption/gameOption';
+import rulesImg from '../../images/image-rules.svg'
 export default function Modal(props) {
     return (
         <div className={`${styles.modal} container-fluid h-100 bg-white`}>
             <h1 className={`${styles.modalTitle} fw-bold`}>RULES</h1>
-            <GameOption hand="rock" mode="rules" />
+            <GameOption hand="rock"/>
         </div>
     )
 }
